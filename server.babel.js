35737af079b0fe01a4628b1c00519a08652a16fc
Forrest Collins <Forest.d.collins@gmail.com>
2017-03-18 21:31:58 -0500

    Added the server.babel file

---
+++ server.babel.js
@@ -0,0 +1,14 @@
+import http from 'http'
+import expressConfig from './server/express'
+
+const ENV = (node.env.PROCESS_ENV || 'development');
+const appConfig = require(`./server/config/${ENV}`);
+
+const app = expressConfig();
+const server = http.createServer(app);
+
+server.listen(appConfig.server.port, () => {
+	console.log(`The application is up and running at ${appConfig.server.host}${appConfig.server.port} and then environment is currently: ${ENV}`);
+});
+
+global.config = appConfig;
